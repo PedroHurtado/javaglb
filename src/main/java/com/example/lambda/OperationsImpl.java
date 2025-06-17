6cacb0deb4bacd2be36502e9afdacc2d0f8f1c70
Pedro Hurtado <pedrohurt@gmail.com>
2025-06-17 13:29:50 +0200

    functional

--- src/main/java/com/example/lambda/OperationsImpl.java
+++ src/main/java/com/example/lambda/OperationsImpl.java
@@ -3,10 +3,14 @@ package com.example.lambda;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 
 public class OperationsImpl {
     public static void Operations(){
+        Function<Integer,Function<Integer, Integer>> clousure = a -> b->a+b;
+        Function<Integer,Function<Integer, Integer>> clousure1 = OperationsImpl::clousureReference;
+
         List<BiFunction<Integer, Integer, Integer>> operations = new ArrayList<>();
         //List<Operations<Integer>> operations = new ArrayList<>();
         operations.add(OperationsImpl::sum);
@@ -17,9 +21,21 @@ public class OperationsImpl {
         for (var operation : operations) {
             System.out.println(operation.apply(2, 2));
         }
+
+        var result = clousure.apply(8);
+        System.out.println(result.apply(3)); //11
+        System.out.println(result.apply(100)); //11
+
+        var result1 = clousure1.apply(8);
+        System.out.println(result1.apply(3)); //11
+        System.out.println(result1.apply(100)); //11
+
     }
     public static Integer sum(Integer a, Integer b){
         return a+b;
+    }
+    public static Function<Integer,Integer> clousureReference(final Integer a){
+        return (b)->a+b;
     }
 
 }
