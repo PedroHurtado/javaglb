e4a5b1e054af5386d23bc0eb42ea89b8b3fa600f
Pedro Hurtado <pedrohurt@gmail.com>
2025-06-25 08:57:53 +0200

    CommandContext

--- src/main/java/com/example/paint/commands/CommandContext.java
+++ src/main/java/com/example/paint/commands/CommandContext.java
@@ -1,5 +1,25 @@
 package com.example.paint.commands;
 
+import java.util.Scanner;
+
+import com.example.paint.canvas.Canvas;
+
 public class CommandContext {
-    
+    private final Canvas canvas;
+    private final Scanner scanner;
+    private final Runnable onExit;
+    public CommandContext(Canvas canvas, Scanner scanner, Runnable onExit) {
+        this.canvas = canvas;
+        this.scanner = scanner;
+        this.onExit = onExit;
+    }
+    public Canvas getCanvas() {
+        return canvas;
+    }
+    public Scanner getScanner() {
+        return scanner;
+    }
+    public Runnable getOnExit() {
+        return onExit;
+    }
 }
