769c35733137b3f7e60e77739da0cdc3a942964d
Pedro Hurtado <pedrohurt@gmail.com>
2025-06-26 11:13:29 +0200

    refactoring

--- src/main/java/com/example/paint/core/CommandRegistry.java
+++ src/main/java/com/example/paint/core/CommandRegistry.java
@@ -1,12 +1,12 @@
 package com.example.paint.core;
 
 import org.reflections.Reflections;
-
 import com.example.paint.commands.interfaces.CommandFactory;
 
 import java.util.*;
 
 public class CommandRegistry {
+
     private static final Map<String, CommandEntry> registry = new LinkedHashMap<>();
 
     public static class CommandEntry {
@@ -19,24 +19,35 @@ public class CommandRegistry {
         }
     }
 
-    public static void registerCommands(Reflections reflections) {        
-        
-        Set<Class<?>> commandFactories = reflections.getTypesAnnotatedWith(RegisterCommand.class);
+    public static void registerCommands(Reflections reflections) {
+        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(RegisterCommand.class);
 
-        for (Class<?> clazz : commandFactories) {
+        for (Class<?> clazz : annotatedClasses) {
+            if (!isValidCommandFactory(clazz)) continue;
+
             RegisterCommand annotation = clazz.getAnnotation(RegisterCommand.class);
-            if (!CommandFactory.class.isAssignableFrom(clazz)) continue;
+            Optional<CommandFactory> factory = createFactoryInstance(clazz);
 
-            try {
-                CommandFactory factory = (CommandFactory) clazz.getDeclaredConstructor().newInstance();
-                registry.put(annotation.value(), new CommandEntry(factory, annotation.description()));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            factory.ifPresent(cmdFactory -> 
+                registry.put(annotation.value(), new CommandEntry(cmdFactory, annotation.description()))
+            );
+        }
+    }
+
+    private static boolean isValidCommandFactory(Class<?> clazz) {
+        return CommandFactory.class.isAssignableFrom(clazz);
+    }
+
+    private static Optional<CommandFactory> createFactoryInstance(Class<?> clazz) {
+        try {
+            return Optional.of((CommandFactory) clazz.getDeclaredConstructor().newInstance());
+        } catch (Exception e) {
+            System.err.printf("Error instantiating CommandFactory for class %s: %s%n", clazz.getName(), e.getMessage());
+            return Optional.empty();
         }
     }
 
     public static Map<String, CommandEntry> getCommands() {
-        return registry;
+        return Collections.unmodifiableMap(registry);
     }
 }
