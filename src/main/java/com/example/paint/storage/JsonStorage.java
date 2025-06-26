424374937e9606de7570a39c48991cf897793439
Pedro Hurtado <pedrohurt@gmail.com>
2025-06-26 12:30:32 +0200

    implements storage methods

--- src/main/java/com/example/paint/storage/JsonStorage.java
+++ src/main/java/com/example/paint/storage/JsonStorage.java
@@ -1,27 +1,39 @@
 package com.example.paint.storage;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import com.example.paint.shapes.Shape;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class JsonStorage implements Storage{
+public class JsonStorage implements Storage {
     private final ObjectMapper mapper;
-    
+
     public JsonStorage(ObjectMapper mapper) {
         this.mapper = mapper;
     }
 
     @Override
     public void save(String filename, List<Shape> shapes) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(filename), shapes.toArray(new Shape[0]));
+        } catch (IOException e) {
+            throw new RuntimeException("Error saving shapes to file", e);
+        }
     }
 
     @Override
     public List<Shape> load(String filename) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'load'");
+
+        try {
+            return mapper.readValue(new File(filename), new TypeReference<List<Shape>>() {
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading shapes from file", e);
+        }
     }
-    
+
 }
